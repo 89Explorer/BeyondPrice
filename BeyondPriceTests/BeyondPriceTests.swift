86065fc28a22fb733b86e811653f364d0603610a
89Explorer <leral3@naver.com>
2024-12-16 21:42:29 +0900

    Initial Commit

---
+++ BeyondPriceTests/BeyondPriceTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondPriceTests.swift
+//  BeyondPriceTests
+//
+//  Created by 권정근 on 12/16/24.
+//
+
+import Testing
+@testable import BeyondPrice
+
+struct BeyondPriceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
