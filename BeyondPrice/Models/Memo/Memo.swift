8df9145bf7b9c7d31e190753c66f50d14cc38a05
89Explorer <leral3@naver.com>
2024-12-18 16:24:27 +0900

    Feat: 메모, 메모매니저 데이터 모델 생성

---
+++ BeyondPrice/Models/Memo/Memo.swift
@@ -0,0 +1,33 @@
+//
+//  Memo.swift
+//  BeyondPrice
+//
+//  Created by 권정근 on 12/18/24.
+//
+
+import Foundation
+import UIKit
+
+
+// Memo 클래스 정의
+class Memo: Codable {
+    
+    var id: String
+    var date: Date
+    var weather: String
+    var title: String
+    var content: String
+    var category: String
+    var image: [String]?
+
+    init(id: String = UUID().uuidString, date: Date, weather: String, title: String, content: String, category: String, image: [String]? = nil) {
+        self.id = id
+        self.date = date
+        self.weather = weather
+        self.title = title
+        self.content = content
+        self.category = category
+        self.image = image
+    }
+    
+}
---
+++ BeyondPrice/Models/Memo/MemoManager.swift
@@ -0,0 +1,48 @@
+//
+//  MemoManager.swift
+//  BeyondPrice
+//
+//  Created by 권정근 on 12/18/24.
+//
+
+import Foundation
+
+protocol MemoCRUD {
+    func addMemo(_ newMemo: Memo)
+    func updateMemo(_ updateMemo: Memo)
+    func deleteMemo(with memoId: String)
+    func getMemo(with memoId: String) -> Memo?
+}
+
+// MemoManager 클래스 정의
+class MemoManager: MemoCRUD, Codable {
+
+    private var memos: [Memo] = []
+    
+    // 메모 추가
+    func addMemo(_ newMemo: Memo) {
+        memos.append(newMemo)
+    }
+    
+    func updateMemo(_ updateMemo: Memo) {
+        for (index, memo) in memos.enumerated() {
+            if memo.id == updateMemo.id {
+                memos[index] = updateMemo
+                break
+            }
+        }
+    }
+    
+    func deleteMemo(with memoId: String) {
+        memos.removeAll(where: { memo in
+            return memo.id == memoId
+        })
+    }
+    
+    func getMemo(with memoId: String) -> Memo? {
+        return memos.first(where: { memo in
+            return memo.id == memoId
+        })
+    }
+    
+}
